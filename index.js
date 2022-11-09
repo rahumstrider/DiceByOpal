8060552f649d1101aa0e4d810f0761604766be93
AniruthXI <32874665+rahumstrider@users.noreply.github.com>
2022-11-09 22:59:47 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+window.alert("Refresh me!");
+
+
+// ชื่อผู้เล่น
+var player1 = "Player 1";
+var player2 = "Player 2";
+
+// สำหรับเปลี่ยนชื่อผู้เล่น
+function editNames() {
+    player1 = prompt("Change Player1 name");
+    player2 = prompt("Change player2 name");
+
+    document.querySelector("p.Player1")
+                    .innerHTML = player1;
+                    
+    document.querySelector("p.Player2")
+                    .innerHTML = player2;
+}
+
+// ฟังค์ชั่นทอยลูกเต๋า
+function rollTheDice() {
+    setTimeout(function () {
+        var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+        var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+        document.querySelector(".img1").setAttribute("src",
+            "dice" + randomNumber1 + ".png");
+
+        document.querySelector(".img2").setAttribute("src",
+            "dice" + randomNumber2 + ".png");
+
+        if (randomNumber1 === randomNumber2) {
+            document.querySelector("h1").innerHTML = "Draw!";
+        }
+
+        else if (randomNumber1 < randomNumber2) {
+            document.querySelector("h1").innerHTML
+                = (player2 + " Wins!");
+        }
+
+        else {
+            document.querySelector("h1").innerHTML
+                = (player1 + " Wins!");
+        }
+    });
+}   
